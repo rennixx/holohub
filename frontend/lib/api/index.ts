aebb9feb4ca410782a1dbf71ca78ce66091a0b0a
rennixx <ensw05323@student.su.edu.krd>
2026-01-27 03:07:02 +0300

    feat: export billing API module in API barrel

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -5,6 +5,7 @@
 export { apiClient as default } from "./client";
 export * from "./auth";
 export * from "./assets";
+export * from "./billing";
 export * from "./devices";
 export * from "./organizations";
 export * from "./playlists";
